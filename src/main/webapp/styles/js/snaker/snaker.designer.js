5417ccd8ce4ebf31ac6cf74cec7c2921fd4e810f
snakerflow <snakerflow@163.com>
2014-12-05 23:34:13 +0800

    去除表单弹出js

--- src/main/webapp/styles/js/snaker/snaker.designer.js
+++ src/main/webapp/styles/js/snaker/snaker.designer.js
@@ -221,12 +221,12 @@
             if (!designer.config.editable) {
                 designer.util.tip(_rect, _o.props['name'].value);
             } else {
-                var returnValue = window.showModalDialog(designer.config.ctxPath + '/config/form?lookup=1',window,'dialogWidth:1000px;dialogHeight:600px');
+/*                var returnValue = window.showModalDialog(designer.config.ctxPath + '/config/form?lookup=1',window,'dialogWidth:1000px;dialogHeight:600px');
                 if(returnValue) {
                     var formPath = "/config/form/use/" + returnValue;
                     _o.props.form.value = formPath;
                     document.getElementById("pform").innerHTML = '<input style="width:98%;" value="' + formPath + '"/>';
-                }
+                }*/
             }
         });
         _rect.click(function(){
@@ -236,12 +236,12 @@
         });
         _rect.dblclick(function(){
             if (designer.config.editable) {
-                var returnValue = window.showModalDialog(designer.config.ctxPath + '/config/form?lookup=1',window,'dialogWidth:1000px;dialogHeight:600px');
+/*                var returnValue = window.showModalDialog(designer.config.ctxPath + '/config/form?lookup=1',window,'dialogWidth:1000px;dialogHeight:600px');
                 if(returnValue) {
                     var formPath = "/config/form/use/" + returnValue;
                     _o.props.form.value = formPath;
                     document.getElementById("pform").innerHTML = '<input style="width:98%;" value="' + formPath + '"/>';
-                }
+                }*/
             }
         });
         _img.drag(function(dx, dy) {
